780a5851fe3842dcb039511840d4fd863fedb0bc
unwary <codeberg.hamburger849@simplelogin.com>
2025-01-06 03:56:42 +0100

    stuff works *for now*

--- nixosModules/development.nix
+++ nixosModules/development.nix
@@ -1,89 +1,75 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
+{lib,  pkgs,  config, ... }:
 
 let
-  cfg-desktop = config.my.desktop.my.desktop.enableDesktopSpecific;
-  cfg-universial = config.my.desktop.enableUniversial;
+  cfg = config.my.desktop;
+  inherit (lib.lists) optionals;
+
 in
 {
-  imports = [
+  options.my.desktop = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      example = true;
+    };
+
+  };
+
+ imports =
+  [
     ../softwareconf/doom-emacs.nix
     ../softwareconf/nvim.nix
   ];
+  config={
+    environment.systemPackages = [
 
-  options.my.desktop = {
-    enableDesktopSpecific = lib.mkEnableOption "Desktop";
-    enableUniversial = lib.mkEnableOption "Universial";
-  };
-  config = lib.mkMerge [
-    (lib.mkIf cfg-universial.enable {
+      #Docker
+      pkgs.lazydocker
 
-      environment.systemPackages = lib.attrValues {
-        inherit (pkgs)
-          jetbrains-toolbox
-          vscodium-fhs
-          mariadb
-          nodejs
-          jdk11
-          jdk
-          google-java-format
-          jdt-language-server
-          ;
-      };
+      # Languages
+      pkgs.shellcheck
+      pkgs.nixd
+      pkgs.nil
+      pkgs.python3
+      pkgs.pipx
+      pkgs.uv
+      #DOOM - Emacs
+      pkgs.shellcheck
+      pkgs.nixd
+      pkgs.nil
+      pkgs.alejandra #Nix-Formatting
+      pkgs.nixfmt
+  ] ++ optionals cfg.enable [
 
-      #nix.nixPath = [ "nixpkgs = $(inputs.nixpkgs)" ];
-      #home-manager.users.${config.my.username} = {
-      #  programs = {
-      #    neovim.enable = true;
-      #    java.enable = true;
-      #  };
+      pkgs.jetbrains-toolbox
+      pkgs.vscodium-fhs
+      pkgs.mariadb
+      pkgs.nodejs
+      pkgs.jdk11
+      pkgs.jdk
+      pkgs.google-java-format
+      pkgs.jdt-language-server # cool suggestions
 
-      #  #programs.doom-emacs = {
-      #  #  enable = true;
-      #  #};
-      #};
-
-      #virtualisation.docker.enable = true;
-
-    })
-    (lib.mkIf (cfg-universial.enable && cfg-desktop.enable) {
-#      environment.systemPackages = lib.attrValues {
-#        inherit (pkgs)
-#          # Docker
-#          lazydocker
-#          # Lanuages
-#
-#          ## DOOM - Emacs
-#          shellcheck
-#          nixd # Nix-Lsp
-#          nil
-#          alejandra # Nix-Formatting
-#          nixfmt
-#
-#          ## Python
-#          python3
-#          pipx
-#          uv
-#
-#          ;
-#      };
-#      nix.nixPath = [ "nixpkgs = $(inputs.nixpkgs)" ];
-#      home-manager.users.${config.my.username} = {
-#        programs = {
-#          lazygit.enable = true;
-#          ripgrep.enable = true; # For doom-emacs
-#        };
-
-        #programs.doom-emacs = {
-        #  enable = true;
-        #};
-#      };
-
-    })
   ];
 
+
+  nix.nixPath = ["nixpkgs = $(inputs.nixpkgs)"];
+    home-manager.users.${config.my.username}={
+      programs={
+        neovim.enable =true;
+        java.enable = true;
+        git.enable = true;
+        lazygit.enable = true;
+        ripgrep.enable = true; # For doom-emacs
+
+
+    };
+
+      #programs.doom-emacs = {
+        #  enable = true;
+        #};
+  };
+  virtualisation.docker.enable = true;
+
+  };
 }
